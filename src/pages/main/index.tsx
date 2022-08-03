2b2f376c703b5c4e9eb83165b790bda048402e57
Silentvoician1346 <silentvoician1346@gmail.com>
2022-08-03 16:55:26 +0700

    add initial structure

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Main() {
-  return <div>index</div>;
+  return (
+    <main>
+      <header className="">PokeDex</header>
+      <div>
+        <section>
+          {/* // todo filter side section */}
+          Filter Section
+        </section>
+        <section>
+          <div>
+            {/* // todo search bar */}
+            Search
+          </div>
+          <div>
+            {/* // todo search result grid */}
+            Result
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default Main;
